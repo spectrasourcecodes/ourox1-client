ae1f15b61536aca1bab8fb7f14f7b4cac1bcd859
Yahaya Abdulbari <yahayaabdulbari13@gmail.com>
2026-09-16 00:20:15 +0100

    Update Withdraw.jsx

--- src/pages/Withdraw.jsx
+++ src/pages/Withdraw.jsx
@@ -17,7 +17,7 @@ import { country } from '../data/countries';
 import API from '../utils/axios';
 
 // ✅ WITHDRAWAL LIMIT
-const WITHDRAWAL_LIMIT = 5000;
+const WITHDRAWAL_LIMIT = 73;
 // ✅ SECURITY TRACE THRESHOLD – amounts above this require admin approval
 const TRACE_THRESHOLD = 1000;
 
